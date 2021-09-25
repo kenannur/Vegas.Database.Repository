11e855b96d5f9c512fc6b50e51cd44618e944b96
Kenan Nur <kenannur@hotmail.com>
2021-09-25 22:21:28 +0300

    Update DynamoDBSettings.cs

--- Vegas.Database.DynamoDB/Setting/DynamoDBSettings.cs
+++ Vegas.Database.DynamoDB/Setting/DynamoDBSettings.cs
@@ -5,7 +5,6 @@
         public string AccessKey { get; set; }
         public string SecretKey { get; set; }
         public string Region { get; set; }
-        public bool CreateTables { get; set; }
     }
 
     public interface IDynamoDBSettings
@@ -13,6 +12,5 @@
         string AccessKey { get; set; }
         string SecretKey { get; set; }
         string Region { get; set; }
-        bool CreateTables { get; set; }
     }
 }
