45aeb655785c199386fc21e855f0e6fa4f2fa870
Kenan Nur <kenannur@hotmail.com>
2021-09-25 23:41:20 +0300

    AmazonDynamoDBClientExtensions added

--- Vegas.Database.DynamoDB/Repository/IDynamoAsyncRepository.cs
+++ Vegas.Database.DynamoDB/Repository/IDynamoAsyncRepository.cs
@@ -1,18 +1,9 @@
-﻿using System.Collections.Generic;
-using System.Reflection;
-using System.Threading.Tasks;
-using Vegas.Database.Abstraction.Repository;
+﻿using Vegas.Database.Abstraction.Repository;
 using Vegas.Database.DynamoDB.Entity;
 
 namespace Vegas.Database.DynamoDB.Repository
 {
     public interface IDynamoAsyncRepository<TEntity> : IAsyncRepository<TEntity, string>
         where TEntity : DynamoEntity
-    {
-        Task<List<string>> GetTablesAsync();
-
-        Task CreateTablesAsync(Assembly assembly);
-
-        Task DeleteTablesAsync();
-    }
+    { }
 }
