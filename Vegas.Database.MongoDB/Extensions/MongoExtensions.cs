68fa3d4c90875ecac745def05f20d7e16e9f8dcc
Kenan Nur <kenannur@hotmail.com>
2021-10-24 00:00:08 +0300

    MongoExtensions created

---
+++ Vegas.Database.MongoDB/Extensions/MongoExtensions.cs
@@ -0,0 +1,23 @@
+﻿using MongoDB.Bson;
+using MongoDB.Driver;
+
+namespace Vegas.Database.MongoDB.Extensions
+{
+    public static class MongoExtensions
+    {
+        public static ObjectId? ToObjectId(this string str)
+            => ObjectId.TryParse(str, out ObjectId objectId) ? objectId : null;
+
+        public static IFindFluent<TEntity, TEntity> NextPage<TEntity>(
+            this IFindFluent<TEntity, TEntity> queryable, int? pageNumber, int? pageCount)
+        {
+            if (!pageNumber.HasValue || !pageCount.HasValue)
+            {
+                return queryable;
+            }
+            return queryable
+                .Skip((pageNumber.Value - 1) * pageCount.Value)
+                .Limit(pageCount.Value);
+        }
+    }
+}
